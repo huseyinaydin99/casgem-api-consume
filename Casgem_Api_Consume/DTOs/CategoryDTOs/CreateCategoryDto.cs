5497efa04444cbc26e781b3c1128ea5dce1a12ba
huseyinaydin99 <huseyinaydin99@gmail.com>
2023-07-20 09:43:50 +0300

    Add project files.

---
+++ Casgem_Api_Consume/DTOs/CategoryDTOs/CreateCategoryDto.cs
@@ -0,0 +1,7 @@
+﻿namespace Casgem_Api_Consume.DTOs.CategoryDTOs
+{
+    public class CreateCategoryDto
+    {
+        public string CategoryName { get; set; }
+    }
+}
---
+++ Casgem_Api_Consume/DTOs/CategoryDTOs/ResultCategoryDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Casgem_Api_Consume.DTOs.CategoryDTOs
+{
+    public class ResultCategoryDto
+    {
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+    }
+}
